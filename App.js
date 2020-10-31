16e829f161263da2fc4f562d9f65eaa9a8232e64
Anderson Teala <andersonteala@gmail.com>
2020-10-31 12:15:03 -0300

    Styles and groups

--- App.js
+++ App.js
@@ -1,35 +1,32 @@
 import React, { Component } from 'react';
-import {View, Text, Button, StatusBar } from 'react-native'
+import {View, Text, StyleSheet } from 'react-native'
 
 class App extends Component{
-
-  constructor(props){
-    super(props);
-    this.state = {
-      nome: ''
-    };
-
-    this.entrar = this.entrar.bind(this);
-  }
-
-  entrar(nome){
-    this.setState({
-      nome: nome
-    })
-  }
-
-  render(){
+render(){
     return(
-        <View style={{ marginTop: 120}}>
+        <View style={styles.area} >
 
-          <Button title="Entrar" onPress={ () => this.entrar('Alterar Texto')} />
+          <Text style={[ styles.textoPrincipal, styles.alinhaTexto ]} >Eu sou o texto 1</Text>
+          <Text style={styles.alinhaTexto} >Eu sou o texto 2</Text>
+          <Text>Eu sou o texto 3</Text>
+          <Text style={styles.textoPrincipal} >Eu sou o texto 4</Text>
 
-          <Text style={{ fontSize: 23, color: '#50fa7b', textAlign: 'center' }}> 
-            {this.state.nome}
-          </Text>
         </View>
       );
     }
   }
+
+  const styles = StyleSheet.create({
+    area:{
+      marginTop: 40
+    },
+    textoPrincipal:{
+      fontSize: 25,
+      color: '#ff5555'
+    },
+    alinhaTexto:{
+      textAlign: 'center'
+    }
+  });
 
 export default App;
