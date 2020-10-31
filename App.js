ccdd4681a6f5204b5cc931cdb3bf3968e1969159
Anderson Teala <andersonteala@gmail.com>
2020-10-31 11:55:45 -0300

    test with states

--- App.js
+++ App.js
@@ -1,43 +1,35 @@
 import React, { Component } from 'react';
-import {View, Text, Image} from 'react-native'
+import {View, Text, Button, StatusBar } from 'react-native'
 
 class App extends Component{
-  render(){
 
-    let nome = 'Retr0';
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: ''
+    };
 
-    return(
-      <View>
-        <Text>Hello World!</Text>
-        <Text>Meu Primeiro App</Text>
-        <Text style= {{ color: '#ff5555', fontSize: 25, margin: 15 }}>
-          Anderson Teala
-          </Text>
-
-      <Text style={{ fontSize: 30 }}> {nome} </Text>
-
-      <Anderson Largura={100} altura={100} fulano="Teala" />
-
-      </View>
-    );
+    this.entrar = this.entrar.bind(this);
   }
-}
+
+  entrar(nome){
+    this.setState({
+      nome: nome
+    })
+  }
+
+  render(){
+    return(
+        <View style={{ marginTop: 120}}>
+
+          <Button title="Entrar" onPress={ () => this.entrar('Alterar Texto')} />
+
+          <Text style={{ fontSize: 23, color: '#50fa7b', textAlign: 'center' }}> 
+            {this.state.nome}
+          </Text>
+        </View>
+      );
+    }
+  }
 
 export default App;
-
-class Anderson extends Component{
-  render(){
-
-    let img = 'https://blogatnteala.sentidoconsultoria.com.br/wp-content/uploads/2020/10/image.jpeg';
-
-    return(
-      <View>
-      <Image 
-        source={{ uri: img }}
-        style={{ width: this.props.Largura, height: this.props.altura }} 
-        />
-        <Text> {this.props.fulano} </Text>
-      </View>
-    );
-  }
-}
